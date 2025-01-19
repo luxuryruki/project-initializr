4fa4d66f83c37790f1a3e8749ed639a53e758e9b
강지효 <gangjihyo@gangjihyoui-MacBookPro.local>
2025-01-19 14:58:30 +0900

    first commit

---
+++ src/main/java/com/project/project_initializr/ProjectInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.project.project_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectInitializrApplication.class, args);
+	}
+
+}
